5c0204f13d5c9c59d995ce637051b8c8543091b1
Jonathan Wang <jonathan.wang01@sap.com>
2022-06-19 21:35:11 +0800

    generate jwt token

--- src/main/java/com/example/oauth2/server/config/AuthServerConfig.java
+++ src/main/java/com/example/oauth2/server/config/AuthServerConfig.java
@@ -1,19 +1,33 @@
 package com.example.oauth2.server.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthServerConfig
         extends AuthorizationServerConfigurerAdapter {
+    @Value("${password}")
+    private String password;
 
+    @Value("${privateKey}")
+    private String privateKey;
+
+    @Value("${alias}")
+    private String alias;
     @Autowired
     private AuthenticationManager authenticationManager;
 
@@ -24,7 +38,7 @@ public class AuthServerConfig
                 .secret("secret")
                 .authorizedGrantTypes("authorization_code", "refresh_token")
                 .scopes("read")
-                .redirectUris("http://localhost:9090/login/oauth2/code/client");
+                .redirectUris("http://localhost:9090/login/oauth2/code/client","https://www.baidu.com");
     }
 
 //    @Override
@@ -45,7 +59,27 @@ public class AuthServerConfig
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints.authenticationManager(authenticationManager)
+                .tokenStore(tokenStore())
+                .accessTokenConverter(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        var converter = new JwtAccessTokenConverter();
+
+        KeyStoreKeyFactory keyStoreKeyFactory =
+                new KeyStoreKeyFactory(
+                        new ClassPathResource(privateKey),
+                        password.toCharArray());
+        converter.setKeyPair(keyStoreKeyFactory.getKeyPair(alias));
+
+        return converter;
     }
 
     @Override
